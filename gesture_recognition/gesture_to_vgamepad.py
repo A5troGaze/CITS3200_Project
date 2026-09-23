e59b142f0cf1bad6fac73d142b3e5d8f5f8172eb
chris.wes <christoffel.wesseloo@gmail.com>
2026-09-24 07:25:14 +0800

    changed turn velocity to 0.75 for testing. didnt do anything

--- gesture_recognition/gesture_to_vgamepad.py
+++ gesture_recognition/gesture_to_vgamepad.py
@@ -32,8 +32,8 @@ GESTURE_CMD = {
     # Bumped from 0.2 -> 0.4: forward/back/strafe (0.3-0.5) all worked, but
     # turning didn't -- 0.2 was likely sitting right in the right stick's
     # deadzone. Tune further if this overshoots or still doesn't register.
-    "turn_right":                (0.0,  0.0, -0.4),
-    "turn_left":                 (0.0,  0.0,  0.4),
+    "turn_right":                (0.0,  0.0, -0.75),
+    "turn_left":                 (0.0,  0.0,  0.75),
     "move_right":                (0.0, -0.3,  0.0),
     "move_left":                 (0.0,  0.3,  0.0),
     "move_forward_left_hand":    (0.5,  0.0,  0.0),
